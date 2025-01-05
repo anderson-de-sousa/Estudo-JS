75cec1cfec72aa17723642e6c2d77e0bf0c99a6d
Anderson de Sousa <itimes.digital@gmail.com>
2025-01-05 14:39:23 -0300

    ajuste de codigo

--- 1_Fundamentos/js/ehDivisivel.js
+++ 1_Fundamentos/js/ehDivisivel.js
@@ -1,10 +1,10 @@
-const valorA = 30;
+const valorA = 31;
 const valorB = 3;
 
 function ehDivisivel(valorA, valorB){
-    return (valorA % valorB) == 0 ? true : false;
+    return (valorA % valorB) === 0 ? true : false;
 }
 
 let resultado = ehDivisivel(valorA, valorB);
 
-console.info(`Os valores ${valorA} e ${valorB} são divisíveis: ${resultado}`);
+console.info(`Os valores ${valorA} e ${valorB} são divisíveis: ${resultado ? "Sim" : "Não"}`);
